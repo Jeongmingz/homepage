46e4d25b1ebe3eab4e56e1bee214553e005a815c
Jeongmingz <joungminyt@gmail.com>
2025-01-31 16:14:04 +0900

    bug: Theme Toggle 버튼이 고정되어있지 않은 문제
    position: absolute -> position: fixed 변경

--- src/_components/ThemeToggle.tsx
+++ src/_components/ThemeToggle.tsx
@@ -6,10 +6,10 @@ const Container = styled.div`
   align-items: center;
   justify-content: center;
 
-  position: absolute;
+  position: fixed;
 
   right: 8px;
-  bottom: 12px;
+  bottom: 20px;
 `;
 
 const ToggleLabel = styled.label`
